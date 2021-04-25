5856a0a6ff1c722e1447c4d7eaa00ab0de561d4c
Tyler Kneidl <tskneidl@gmail.com>
2021-04-25 13:16:37 -0700

    added stylesheet, add to library button and add to wishlist button

--- src/components/SearchAndAdd.jsx
+++ src/components/SearchAndAdd.jsx
@@ -13,9 +13,15 @@ const SearchAndAdd = ()=>{
         axios(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyCG4CetKRxfceKIXmPWjbo5PsOtJ0HOi0U`
         ).then(data=>data.data.items).then(data=>data.forEach(el => {
             const books = [];
-            books.push([el.volumeInfo.title, el.volumeInfo.authors[0], el.volumeInfo.imageLinks.thumbnail]) 
-            
-            setResults(results => [...results, <ShowBooks title = {books[0][0]} author={books[0][1]} imgLink = {books[0][2]}/>])
+            books.push([el.volumeInfo.title, el.volumeInfo.authors[0], el.volumeInfo.imageLinks.thumbnail, el.volumeInfo.industryIdentifiers[1].identifier]) 
+            console.log(data)
+            setResults(results => [...results, 
+            <ShowBooks 
+            title = {books[0][0]} 
+            author={books[0][1]} 
+            imgLink = {books[0][2]}
+            identifier = {books[0][3]}
+            />])
             
         }))
         .catch(e=>console.log(e))
@@ -27,8 +33,10 @@ const SearchAndAdd = ()=>{
         <div>
             <input id='searchBar' type='text' value={search} onChange={e => setSearch(e.target.value)}  />
             <button id='bookSearch' onClick={searchBooks}>Search</button>
-            {results}
-                
+            <div id = 'bookDisplay'>
+                {results}
+            </div>
+            
          
             
             
--- src/components/ShowBooks.jsx
+++ src/components/ShowBooks.jsx
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 const ShowBooks = (props)=>{
 
+    const handleLibraryClick = () =>{
+        axios.post('/ourserverendpoint')
+    }
+
+    const handleWishClick = () =>{
+        axios.post('/ourserverendpoint')
+    }
 
     return(
 
-        <div id='bookDisplay'>
+        <div>
             <h3>{props.title}</h3>
             <img src={props.imgLink}/>
             <h4>{props.author}</h4>
-
+            <button id='addLibrary' onClick = {handleLibraryClick}>I own this!</button>
+            <button id='addWishlist' onClick = {handleWishClick}>I want this!</button>
         </div>
 
     )
