93f877e06c8da6503f6390eb141b1971d4d2da6c
Tyler Kneidl <tskneidl@gmail.com>
2021-04-26 14:02:24 -0700

    latest update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,57 +4,45 @@ import NavBar from "./NavBar.jsx";
 import axios from "axios";
 import CreateAccount from "./CreateAccount.jsx";
 import SearchAndAdd from "./SearchAndAdd.jsx";
-import OrderBooks from "./OrderBooks.jsx"
+import OrderBooks from "./OrderBooks.jsx";
 import Dashboard from "./Dashboard.jsx";
-import Notifications from "./Notifications.jsx"
+import Notifications from "./Notifications.jsx";
 import "../../public/styles.css";
-import { BrowserRouter as Router, Switch, Route, Link  } from 'react-router-dom'
+
+import { navigate, useRoutes } from "@patched/hookrouter";
+
+const routes = {
+  "/": () => <CreateAccount />,
+  "/dashboard": () => <Dashboard />,
+  "/notifications": () => <Notifications />,
+  "/addBooks":  (props) => <SearchAndAdd />,
+  "/orderBooks": () => <OrderBooks />,
+};
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userId, setUserId] = useState('')
-
-  useEffect(() => {
-  }, [isAuthenticated]);
-
-
-  let mainContainer;
-  if (isAuthenticated === true) {
-    mainContainer = <Dashboard />;
-  } else {
-    mainContainer = <CreateAccount />;
-  }
+  const [userId, setUserId] = useState("");
+  const routeResult = useRoutes(routes);
+  useEffect(() => {}, [isAuthenticated]);
+  
 
   return (
-    <Router>
     <div>
       <nav id="navBarContainer">
-        <NavBar logInStatus={isAuthenticated} login={setIsAuthenticated} updateUserId={setUserId} />
+        <NavBar
+          logInStatus={isAuthenticated}
+          login={setIsAuthenticated}
+          updateUserId={setUserId}
+        />
       </nav>
 
-      <div id="mainContainer">
-
-      {/* <Route
-  path='/dashboard'
-  render={(props) => (
-    <Dashboard {...props} isAuthed={true} />
-  )} */}
-       
-        <Switch>
-           <Route exact path='/' component = {CreateAccount}/>
-           <Route path= '/notifcations' component = {Notifications}/>
-           <Route path= '/dashboard' component = {Dashboard} />
-           <Route path='/addBooks' render={(props) => (<SearchAndAdd {...props} userId = {userId} /> )} />
-           <Route path='/orderBooks' component = {OrderBooks} />
-        </Switch>
-        
-      </div>
+      <div id="mainContainer">{routeResult}</div>
     </div>
-    </Router>
   );
 };
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
 
+export const MyContext = React.createContext(defaultValue);
 export default App;
--- src/components/SearchAndAdd.jsx
+++ src/components/SearchAndAdd.jsx
@@ -1,50 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import ShowBooks from './ShowBooks.jsx'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ShowBooks from "./ShowBooks.jsx";
 
+const SearchAndAdd = (props) => {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [add, setAdd] = useState("");
+  const searchBooks = (props) => {
+    console.log("These are our fucking props.",props.userId)
+    setResults([]);
+    axios(
+      `https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyBvzRvXUMUGGeATujnaMUbaQS9dxclLbOk`
+    ).then((data) => data.data.items)
+      .then((data) =>
+        data.forEach((el) => {
+          const books = [];
+          books.push([
+            el.volumeInfo.title,
+            el.volumeInfo.authors[0],
+            el.volumeInfo.imageLinks.thumbnail,
+            el.volumeInfo.industryIdentifiers[1].identifier,
+          ])                 
 
-const SearchAndAdd = ()=>{
-    const [search, setSearch] = useState('');
-    const [results, setResults] = useState([]);
-    const [add, setAdd] = useState('');
-    const searchBooks = (props) => {
-        setResults([]);
-        axios(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyCG4CetKRxfceKIXmPWjbo5PsOtJ0HOi0U`
-        ).then(data=>data.data.items).then(data=>data.forEach(el => {
-            const books = [];
-            books.push([el.volumeInfo.title, el.volumeInfo.authors[0], el.volumeInfo.imageLinks.thumbnail, el.volumeInfo.industryIdentifiers[1].identifier]) 
-            console.log(data)
-            setResults(results => [...results, 
-            <ShowBooks 
-                userId = {props.userId}
-                title = {books[0][0]} 
-                author={books[0][1]} 
-                imgLink = {books[0][2]}
-                id = {books[0][3]}
-            />])
-            
-        }))
-        .catch(e=>console.log(e))
-        console.log(results)
-    }     
-        
+          setResults((results) => [
+            ...results,
+            <ShowBooks
+              userId={props.userId}
+              title={books[0][0]}
+              author={books[0][1]}
+              imgLink={books[0][2]}
+              id={books[0][3]}
+            />
+          ]);
+        })
+      )
+      .catch((e) => console.log(e));
+    console.log(results);
+  };
 
-    return(
-        <div>
-            <input id='searchBar' type='text' value={search} onChange={e => setSearch(e.target.value)}  />
-            <button id='bookSearch' onClick={searchBooks}>Search</button>
-            <div id = 'bookDisplay'>
-                {results}
-            </div>
-            
-         
-            
-            
-        </div>
-    )
-
-
-
-}
+  return (
+    <div>
+      <h1>Hey</h1>
+      <input
+        id="searchBar"
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button id="bookSearch" onClick={searchBooks}>
+        Search
+      </button>
+      <div id="bookDisplay">{results}</div>
+    </div>
+  );
+};
 
 export default SearchAndAdd;
